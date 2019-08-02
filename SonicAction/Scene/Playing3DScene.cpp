b89f7fe611ec5b0eae290a9f175c2175d8ff874e
1701377 <1701377@PC65210>
2019-08-02 13:09:46 +0900

    コミット

--- SonicAction/Scene/Playing3DScene.cpp
+++ SonicAction/Scene/Playing3DScene.cpp
@@ -7,62 +7,96 @@
 Playing3DScene::Playing3DScene(SceneController & controller) :Scene(controller)
 {
 	// pmxファイルを読み込み
-	_modelH = MV1LoadModel("3Dモデル/models/シスタークレア.pmx");
+	_anim.resize(4);
+	_anim[0].modelH = MV1LoadModel("3Dモデル/本間ひまわり/本間ひまわり.pmx");
+	/*_anim[1].modelH = MV1LoadModel("3Dモデル/赤羽葉子/赤羽葉子(修正版).pmx");
+	_anim[2].modelH = MV1LoadModel("3Dモデル/叶/通常衣装/叶.pmx");
+	_anim[3].modelH = MV1LoadModel("3Dモデル/葛葉/葛葉.pmx");*/
+
 	// カメラの設定
-	SetCameraPositionAndTarget_UpVecY(VGet(0.0f, 12.0f, -20.0f), VGet(0.0f, 10.0f, 1.0f));
-	SetCameraNearFar(0.42f, 103.0f);
+	_camera = std::make_shared<Camera3D>();
+	_camera->eye = VGet(0, 25.0f, -20.0f);
+	_camera->target = VGet(0, 25.0f, 1.0f);
+	_camera->viewray = VSub(_camera->target, _camera->eye);
+
+	SetCameraPositionAndTarget_UpVecY(_camera->eye, _camera->target);
+	SetCameraNearFar(0.1f, 100.0f);
+	SetupCamera_Perspective(45.0f*DX_PI_F / 180.0f);
+	for (int i = 0; i < _anim.size(); ++i)
+	{
+		_anim[i].animIdx = MV1AttachAnim(_anim[i].modelH, 0);
+		_anim[i].totalTime = MV1GetAttachAnimTotalTime(_anim[i].modelH, _anim[i].animIdx);
+		MV1SetPosition(_anim[i].modelH, VGet(15.0f*i, 10.0f, 1.0f));
+	}
 }
 
 Playing3DScene::~Playing3DScene()
 {
-	MV1DeleteModel(_modelH);
+	for (int i = 0; i < _anim.size(); ++i)
+	{
+		MV1DeleteModel(_anim[i].modelH);
+	}
 }
 
 void Playing3DScene::Update(const Input & input)
 {
+	for (int i = 0; i < _anim.size(); ++i)
+	{
+		_anim[i].animTime += 0.5f;
+	}
 	if (input.IsTriggered(0, COMMAND::OK))
 	{
 		_controller.ChangeScene(std::make_unique<ResultScene>(_controller));
 	}
-	auto pos = MV1GetPosition(_modelH);
-	auto rad = MV1GetRotationXYZ(_modelH);
+	
 	if (input.IsPressed(0, COMMAND::LEFT))
 	{
-		pos.x -= 1;
+		_camera->viewray.x -= 1.0f;
 	}
 	if (input.IsPressed(0, COMMAND::RIGHT))
 	{
-		pos.x += 1;
+		_camera->viewray.x += 1.0f;
 	}
 	if (input.IsPressed(0, COMMAND::UP))
 	{
-		pos.y += 1;
+		_camera->viewray.y += 1.0f;
 	}
 	if (input.IsPressed(0, COMMAND::DOWN))
 	{
-		pos.y -= 1;
+		_camera->viewray.y -= 1.0f;
 	}
 	if (input.IsPressed(0, COMMAND::BACK))
 	{
-		pos.z += 1;
+		_camera->viewray.z += 1.0f;
 	}
 	if (input.IsPressed(0, COMMAND::FRONT))
 	{
-		pos.z -= 1;
+		_camera->viewray.z -= 1.0f;
 	}
 	if (input.IsPressed(0, COMMAND::ROTATE))
 	{
-		rad.y += 5.0f*DX_PI_F / 180.0f;
+		auto mat = MGetRotY(5.0f*DX_PI_F / 180.0f);
+		_camera->viewray = VTransformSR(_camera->viewray, mat);
 	}
-	if (input.IsPressed(0, COMMAND::JUMP))
+	
+	for (int i = 0; i < _anim.size(); ++i)
 	{
-		MV1AttachAnim(_modelH, 0);
+		if (_anim[i].totalTime <= _anim[i].animTime)
+		{
+			_anim[i].animTime = 0.0f;
+		}
 	}
-	MV1SetPosition(_modelH, pos);
-	MV1SetRotationXYZ(_modelH, rad);
+	for (int i = 0; i < _anim.size(); ++i)
+	{
+		MV1SetAttachAnimTime(_anim[i].modelH, _anim[i].animIdx, _anim[i].animTime);
+	}
+	SetCameraPositionAndTarget_UpVecY(VSub(_camera->eye, _camera->viewray), _camera->target);
 }
 
 void Playing3DScene::Draw()
 {
-	MV1DrawModel(_modelH);
+	for (int i = 0; i < _anim.size(); ++i)
+	{
+		MV1DrawModel(_anim[i].modelH);
+	}
 }
--- SonicAction/Scene/Playing3DScene.h
+++ SonicAction/Scene/Playing3DScene.h
@@ -1,11 +1,30 @@
 #pragma once
 #include "Scene.h"
+#include <vector>
+#include <memory>
+#include "../Geometry.h"
+
+struct Anim
+{
+	int modelH;
+	int animIdx;
+	float animTime;
+	float totalTime;
+};
+
+struct Camera3D
+{
+	VECTOR eye;
+	VECTOR target;
+	VECTOR viewray;
+};
 
 class Playing3DScene :
 	public Scene
 {
 private:
-	int _modelH;
+	std::vector<Anim> _anim;
+	std::shared_ptr<Camera3D> _camera;
 public:
 	Playing3DScene(SceneController& controller);
 	~Playing3DScene();
